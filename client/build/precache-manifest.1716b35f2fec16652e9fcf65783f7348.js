66a5daca081414b732e9c2d1aa4b3bc1ae4a2b6a
Aditya Narayan Pandey <66075804+anpandey17@users.noreply.github.com>
2022-03-15 20:39:23 +0530

    Add files via upload

---
+++ client/build/precache-manifest.1716b35f2fec16652e9fcf65783f7348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b9be1dce507dfbfb446137e9f67e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a778c5635043c851752d",
+    "url": "/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "62ca821103535664027a",
+    "url": "/static/js/2.01d10e2f.chunk.js"
+  },
+  {
+    "revision": "a778c5635043c851752d",
+    "url": "/static/js/main.eac5e68b.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
